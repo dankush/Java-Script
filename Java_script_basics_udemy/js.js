2b44aaeade0c8dbc74f904dcf38ccba559b7e04f
dan_kushner <dan.k0288@gmail.com>
2016-03-06 20:48:02 +0200

    Importent books

---
+++ Java_script_basics_udemy/js.js
@@ -0,0 +1,31 @@
+
+/*
+document.write("This is our javascript being written first before anything else\n");
+
+var lives=3
+
+document.write(lives+"\n");
+
+
+var VisitorAge = prompt("what is your age?")
+
+document.write(VisitorAge);
+
+*/
+var hotText = 'MDN';
+
+var URL = 'https://developer.mozilla.org/';
+
+document.write('Click to return to ' + hotText.link(URL));
+
+
+function test() {
+  var i = 3;
+  print(i+2);
+}
+
+
+console.log('Click to return to ' + hotText.link(URL));
+// Click to return to <a href="https://developer.mozilla.org/">MDN</a>
+
+//console.log("This is text that will be sent directly to the console to let us know that our program completed successfully");
